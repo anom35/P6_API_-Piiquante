aedc24e4542e7e6a9a5bd8595a349de905cb5729
anom35 <freelance35830@gmail.com>
2023-02-03 16:29:36 +0100

    Supprime console.log

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -19,12 +19,10 @@ exports.getOneSauce = (req, res, next) => {
 exports.createSauce = (req, res, next) => {
 	const sauceObject = JSON.parse(req.body.sauce);
 	delete sauceObject._id;
-	console.log(sauceObject);
 	const sauce = new Sauce({
 		...sauceObject,
 		imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
 	});
-	console.log(sauce);
 	sauce
 		.save()
 		.then(() => res.status(201).json({ message: 'Sauce enregistré !' }))
