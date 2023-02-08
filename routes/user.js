886e39cc3b7e532b1dca42fc029e1e0e668018b3
anom35 <freelance35830@gmail.com>
2023-02-08 15:36:36 +0100

    commente user route

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
+// route pour la création d'utilisateur ou le login
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
